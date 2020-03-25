765f605fff29cb90d1042b1fc1837c0741fe9943
ninthnails <ninth.nails@gmail.com>
2020-03-25 14:18:51 -0400

    Add CMAK (Cluster Manager for Apache Kafka) module to AMI

---
+++ image/packer/ansible/roles/cmak/templates/usr/local/sbin/setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+
+/usr/bin/sed -i -r "s/^127.0.0.1(.*)$/127.0.0.1\1 $(/usr/bin/hostname)/" /etc/hosts
+
+get_ssm_parameter() {
+  /usr/bin/aws ssm --region "${AWS_REGION}" get-parameter --name "${1}" --query Parameter.Value --output text
+}
+
+get_sm_secret() {
+  /usr/bin/aws --region "${AWS_REGION}" secretsmanager get-secret-value --secret-id "${1}" --query SecretString --output text
+}
+
+setup_cruise_control() {
+  /usr/bin/sed -r -i \
+    -e "s|^bootstrap\.servers=.*|bootstrap.servers=${KAFKA_BOOTSTRAP_SERVERS}|gm" \
+    -e "s|^zookeeper\.connect=.*|zookeeper.connect=${KAFKA_ZOOKEEPER_CONNECT}|gm" \
+    -e "s|^sample\.store\.topic\.replication\.factor=.*|sample.store.topic.replication.factor=${CRUISE_CONTROL_TOPIC_REPLICATION_FACTOR}|gm" \
+    /opt/cruise-control/config/cruisecontrol.properties
+
+    /bin/echo "${CRUISE_CONTROL_CAPACITY}" > /opt/cruise-control/config/capacity.json
+
+    /bin/echo "${CLUSTER_ENVIRONMENT},${CLUSTER_NAME},${CRUISE_CONTROL_API_ENDPOINT}" > \
+      /opt/cruise-control/cruise-control-ui/dist/static/config.csv
+
+    if [ "${CRUISE_CONTROL_ENABLED}" == "true" ]; then
+      /usr/bin/systemctl enable cruisecontrol.service
+      /usr/bin/systemctl start cruisecontrol
+    fi
+}
+
+add_cmak_env() {
+  /bin/echo "${1}='${2}'" >> {{ cmak_install_path }}/conf/environment
+}
+
+setup_cmak() {
+  /usr/bin/sed -r -i -e "s|^ZK_HOSTS=.*|ZK_HOSTS=${ZOOKEEPER_CONNECT}|gm" {{ cmak_install_path }}/conf/environment
+
+  local km_pswd="${KAFKA_MANAGER_PASSWORD}"
+  case "${km_pswd}" in
+  parameter/*) km_pswd="$(get_ssm_parameter "${km_pswd/parameter\//}")" ;;
+  secrets/*) km_pswd="$(get_sm_secret "${km_pswd/secrets\//}")" ;;
+  esac
+
+  add_cmak_env KAFKA_MANAGER_AUTH_ENABLED "${KAFKA_MANAGER_AUTH_ENABLED}"
+  add_cmak_env KAFKA_MANAGER_USERNAME "${KAFKA_MANAGER_USERNAME}"
+  add_cmak_env KAFKA_MANAGER_PASSWORD "${km_pswd}"
+
+  /usr/bin/systemctl enable cmak.service
+  /usr/bin/systemctl start cmak
+}
+
+setup_cruise_control
+setup_cmak
